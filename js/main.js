7419ce422838f170454340d797d89056533e53a6
Akramjon <akramjon.ban@gmail.com>
2024-12-13 16:57:45 +0500

    Add all files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+
+
+function myFunction() {
+  let myModal = document.querySelector(".treatment");
+  let val = document.querySelector("#input").value;
+  myModal.style.gridColumnStart = val;
+ // myModal.innerText = ".plant{grid-column-start: "+val +" ;}";
+
+
+}
